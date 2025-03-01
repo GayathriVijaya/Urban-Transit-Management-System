964cc26dd2013b686b4c27a3c9a709404efba917
THIRUMURUGAN <125580604+THIRU-1074@users.noreply.github.com>
2025-03-01 12:03:02 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,6 +15,7 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+
 app.use((req, res, next) => {
     console.log(`${req.method} request for '${req.url}'`);
     next();
